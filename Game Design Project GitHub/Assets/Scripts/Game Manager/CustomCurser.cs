f8a4e979332f5058da6c843e83df83b24c1a1495
DevP0910 <sm1022028@seamedu.in>
2023-10-08 17:10:29 +0530

    idk

--- Game Design Project GitHub/Assets/Scripts/Game Manager/CustomCurser.cs
+++ Game Design Project GitHub/Assets/Scripts/Game Manager/CustomCurser.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CustomCurser : MonoBehaviour
 {
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
