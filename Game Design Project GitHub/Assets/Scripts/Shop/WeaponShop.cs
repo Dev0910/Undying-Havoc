46d8084c3e59387648d1c4ee05b0911613656bef
ArpanChakraborty2003 <arpanchakrabortyslg2003@gmail.com>
2023-10-12 15:29:39 +0530

    Update WeaponShop.cs

--- Game Design Project GitHub/Assets/Scripts/Shop/WeaponShop.cs
+++ Game Design Project GitHub/Assets/Scripts/Shop/WeaponShop.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class WeaponShop : MonoBehaviour
 {
-    bool isBought;
+    //bool isBought;
     private void Start()
     {
-        isBought = false;
+        //isBought = false;
     }
     //buys weapon for the player
     //this function is called by the buttons in UI
     public void BuyWeapon(GameObject prefab)
     {
         //cheaks the conditions to buy the weapon
-        if (prefab.GetComponent<Weapon>().costToBuy <= GameStats.currentGold && isBought == false)
+        if (prefab.GetComponent<Weapon>().costToBuy <= GameStats.currentGold /*&& isBought == false*/)
         {
             GameStats.currentGold -= prefab.GetComponent<Weapon>().costToBuy;//reduse the weapon cost
             GameObject instantiatedPrefab = Instantiate(prefab, GameManager.Instance.player.transform.position, GameManager.Instance.player.transform.rotation);//spawns the weapon
             instantiatedPrefab.transform.parent = GameObject.FindWithTag("WeaponHolder").transform;//set player as the parent gameobject
-            isBought = true;
+            //isBought = true;
         }
 
 
         // Checks the conditions to upgrade the weapon
-        if (prefab.GetComponent<Weapon>().costToUpgrade <= GameStats.currentGold && isBought == true)
+       /* if (prefab.GetComponent<Weapon>().costToUpgrade <= GameStats.currentGold && isBought == true)
         {
             prefab.GetComponent<Weapon>().UpgradeWeapon();
-        }
+        }*/
 
     }
 }
