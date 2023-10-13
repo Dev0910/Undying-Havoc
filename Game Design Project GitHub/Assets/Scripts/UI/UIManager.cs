286a5b7f401b35425b15fa619d9d6fa0232908b7
ArpanChakraborty2003 <arpanchakrabortyslg2003@gmail.com>
2023-10-13 23:11:31 +0530

    Update UIManager.cs

--- Game Design Project GitHub/Assets/Scripts/UI/UIManager.cs
+++ Game Design Project GitHub/Assets/Scripts/UI/UIManager.cs
@@ -7,13 +7,6 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Image playerHealthBar;
-    // Start is called before the first frame update
-   /* void Start()
-    {
-        healthBar.fillAmount = PlayerController.currentHealth / GameManager.Instance.player.GetComponent<PlayerController>().startHealth;
-    }*/
-
-    // Update is called once per frame
     public void UpdatePlayerHP(float maxHP)
     {
         playerHealthBar.fillAmount = PlayerController.currentHealth / maxHP;
