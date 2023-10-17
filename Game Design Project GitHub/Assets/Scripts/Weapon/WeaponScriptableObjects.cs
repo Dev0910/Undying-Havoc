2f9fec01e818cb70b8ddd0758c595a9925d8130b
ArpanChakraborty2003 <arpanchakrabortyslg2003@gmail.com>
2023-10-17 18:03:08 +0530

    Fcuked Up

--- Game Design Project GitHub/Assets/Scripts/Weapon/WeaponScriptableObjects.cs
+++ Game Design Project GitHub/Assets/Scripts/Weapon/WeaponScriptableObjects.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
 public class WeaponScriptableObjects : ScriptableObject
 {
+    public int currentLevel;
     public WeaponData[] weaponsData;
 }
 
 [System.Serializable]
 public class WeaponData
 {
+    //public string name;
     public Sprite weaponSprite;
     public int cost;
     public float damage;
